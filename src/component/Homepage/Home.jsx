612f323515a3f35830014e5314d7ff550255ff66
cyhper1 <firgaming22@gmail.com>
2021-12-16 01:59:45 +0700

    chore(FixLink): Fix URL Link

--- src/component/Homepage/Home.jsx
+++ src/component/Homepage/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
       <h1 className='Information'>Welcome To This bot</h1>
       <h2 className='Information2'>Chyper Primary</h2>
       <header className='ButtonPlay'>
-      <Button href="https://discord.gg/QcCSBnu5es" size="lg"> <SiRobotframework /> Invite Now!</Button>
+      <Button href="https://discord.com/oauth2/authorize?client_id=919444230747873320&scope=bot%20applications.commands&permissions=27648860222" size="lg"> <SiRobotframework /> Invite Now!</Button>
       </header>
     </div>
   )
